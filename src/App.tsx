2041630bdbccf42d381e9d65587a4f93cf7c692b
Ujwal Suwal <rev477r.7@gmail.com>
2026-01-02 15:57:26 +0545

    chore:project + shadcn ui setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <p className='text-red-500 text-2xl'>Check tailwind css</p>
+
+      <Button variant={"default"}>Check</Button>
+    </div>
+  );
+}
+
+export default App;
